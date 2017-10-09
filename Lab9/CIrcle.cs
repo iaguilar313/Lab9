ec489c8a5715cefc8ebc5dc8207b2d36445f8f31
iaguilar313 <iaguilar@umich.edu>
2017-10-09 06:03:16 -0400

    add first

---
+++ Lab9/CIrcle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class CIrcle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            set { radius = value; }
+            get { return radius; }
+        }
+
+
+        public void GetCircumference()
+        {
+            double Circmference = (2 * radius * Math.PI);
+           
+
+            Console.WriteLine("The Circumference" + Circmference);
+
+
+        }
+
+
+        public void GetArea()
+        {
+            double area = Math.PI * (radius * radius);
+           
+            Console.WriteLine( "The area" + area);
+        }
+
+        
+
+        
+ 
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ProgramContinue = false;
+            while (ProgramContinue== false)
+            {
+                Console.WriteLine("Welcome to the Circle Tester");
+                Console.WriteLine("Enter Radius");
+
+
+                CIrcle Object1 = new CIrcle();
+                Object1.Radius = double.Parse(Console.ReadLine());
+
+
+                Object1.GetCircumference();
+
+                Object1.GetArea();
+
+                string UserChoice;
+                Console.WriteLine("Do you want to create a new ciricle");
+                UserChoice = Console.ReadLine();
+
+                if (UserChoice == "No" || UserChoice== "no")
+                {
+                    ProgramContinue = true;
+                }
+     
+            }
+          
+
+
+
+        }
+    }
+}
